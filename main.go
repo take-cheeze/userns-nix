af1776e634e1c4c3a15c2a3999c2ab346d27e25c
take-cheeze <takechi101010@gmail.com>
2025-04-14 18:39:38 +0900

    Escape and add command running functionallity

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ __attribute((constructor(101))) void enter_userns(void) {
 import "C"
 
 import (
+	"al.essio.dev/pkg/shellescape"
+
 	"fmt"
 	"log"
 	"os"
@@ -155,7 +157,12 @@ func main() {
 		log.Panicf("chdir failed: %s", err)
 	}
 
-	cmd := exec.Command("bash", "-c", startScript+"\n"+os.Getenv("SHELL"))
+	cmds := os.Getenv("SHELL")
+	if len(os.Args) > 1 {
+		cmds = " " + shellescape.QuoteCommand(os.Args[1:])
+	}
+
+	cmd := exec.Command("bash", "-c", startScript+"\n"+cmds)
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
